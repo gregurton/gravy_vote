8471f58da3e29e8aefb86bf04bf93d68e45dd720
gregurton <gburt802@gmail.com>
2014-11-04 15:24:44 -0500

    created gravy_vote api project

---
+++ gravy_vote/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+GravyVote::Application.config.secret_token = '83d23017120a68196baea8af3db9a5b05390e30b438042818cc138f794a2772e936e533762238503b56056070ac58b18f212bf6d8cbed77449428d08e04567c3'
